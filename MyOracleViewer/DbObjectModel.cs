596816bd1e228705626111940ea1cc0058d1448b
mohamedibrahim4 <mohamedatlm297@gmail.com>
2025-04-23 23:10:05 +0400

    init

--- MyOracleViewer/DbObjectModel.cs
+++ MyOracleViewer/DbObjectModel.cs
@@ -5,6 +5,7 @@
         public string Type { get; set; }
         public string Name { get; set; }
         public string Schema { get; set; }
+
         public string Package { get; set; }
     }
 }
